2469055bc3f49d1d228f44d9bd4352bb2fcbd6df
jonathan <jola190500@gmail.com>
2025-06-08 16:40:36 +0200

    added migration files

---
+++ backend/Survey.Infrastructure/Migrations/20250507070306_AddSurveyCompletionTypes.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Survey.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSurveyCompletionTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SurveyCompletion_SurveyCompletionType_SurveyCompletionTypeId",
+                table: "SurveyCompletion");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SurveyCompletionType",
+                table: "SurveyCompletionType");
+
+            migrationBuilder.RenameTable(
+                name: "SurveyCompletionType",
+                newName: "SurveyCompletionTypes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SurveyCompletionTypes",
+                table: "SurveyCompletionTypes",
+                column: "SurveyCompletionTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SurveyCompletion_SurveyCompletionTypes_SurveyCompletionType~",
+                table: "SurveyCompletion",
+                column: "SurveyCompletionTypeId",
+                principalTable: "SurveyCompletionTypes",
+                principalColumn: "SurveyCompletionTypeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SurveyCompletion_SurveyCompletionTypes_SurveyCompletionType~",
+                table: "SurveyCompletion");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SurveyCompletionTypes",
+                table: "SurveyCompletionTypes");
+
+            migrationBuilder.RenameTable(
+                name: "SurveyCompletionTypes",
+                newName: "SurveyCompletionType");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SurveyCompletionType",
+                table: "SurveyCompletionType",
+                column: "SurveyCompletionTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SurveyCompletion_SurveyCompletionType_SurveyCompletionTypeId",
+                table: "SurveyCompletion",
+                column: "SurveyCompletionTypeId",
+                principalTable: "SurveyCompletionType",
+                principalColumn: "SurveyCompletionTypeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ backend/Survey.Infrastructure/Migrations/20250511103510_newTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Survey.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class newTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionnairePos",
+                table: "Questionnaires",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuestionnairePos",
+                table: "Questionnaires");
+        }
+    }
+}
